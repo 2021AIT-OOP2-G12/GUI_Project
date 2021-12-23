372f8c1fa3649d740c4ef2295a828aeb5b44b620
YuyaSugisaka <95839239+YuyaSugisaka@users.noreply.github.com>
2021-12-23 18:07:46 +0900

    Update character_search.py
    とりあえず”小路”の部分をハイライトする処理を加えました。

--- character_search.py
+++ character_search.py
@@ -13,12 +13,48 @@ def character_search(name):
     )
     sorce_img = cv2.imread(name)
 
+    #　指定された文字の検索、ハイライト処理 
+    # とりあえず”小路”で実行する
+    str = '小路'
+    str_count = len(str)
+
     for line_box in line_boxs:
-        # 件 名 : バブ リッ クコ メン ト へ の 意見 ((68, 182), (235, 191))
-        print(line_box.content, line_box.position)
-        for word_box in line_box.word_boxes:
-            print('  ', word_box.content, word_box.position)
-            cv2.rectangle(sorce_img, word_box.position[0][:], word_box.position[1][:], (255, 0, 0), thickness=1, lineType=cv2.LINE_8, shift=0)
+        # 件 名 : バブ リッ クコ メン ト へ の 意見 ((68, 182), (235, 191)
+        tate = line_box.position[1][1] - line_box.position[0][1]
+        yoko = (line_box.position[1][0] - line_box.position[0][0])/len(line_box.content)
+        #print(tate)
+        #print(yoko)
+        #print('-----------------!')
+        #print(len(line_box.content))
+
+
+        t = line_box.content.replace(' ', '')
+
+        # 小の左肩の座標
+        #print(line_box.content, line_box.position)
+        if t.find(str) != -1:
+            for s in range(len(line_box.content)):
+                if line_box.content[s] == str[0]:
+                    num_y = line_box.position[0][0]+s*int(yoko)
+                    num_x = line_box.position[0][1]
+
+                    #print('-----------------')
+                    #print(line_box.position[1][1])
+                    #print(num_x)
+                    #print(num_y)
+
+            
+                    for y in range(num_y,num_y+int(yoko)*str_count):
+                       for x in range(num_x,num_x+int(tate)):
+                            b, g, r = sorce_img[x, y]
+                            if (b, g, r) == (255, 255, 255):
+                                continue
+                            sorce_img[x, y] = 0, g, r
+
+
+            #for word_box in line_box.word_boxes:
+                #print('  ', word_box.content, word_box.position)
+                #cv2.rectangle(sorce_img, word_box.position[0][:], word_box.position[1][:], (255, 0, 0), thickness=1, lineType=cv2.LINE_8, shift=0)
     cv2.imwrite('sample.png',sorce_img)
 
 
